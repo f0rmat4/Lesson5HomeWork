9efb5c112c0d49db78108276ff48aa239d179eac
f0rmat4 <f0rmat4you@gmail.com>
2021-02-15 04:22:48 +0300

    Add project files.

---
+++ Homework5.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	int mass[10] = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
+	for (int i : mass) {
+
+		if (int i = 0) i == mass[2, 3, 5, 8, 9]; ++i;
+		mass[i] != 1;
+		cout << mass[i] << endl;
+
+	}
+
+
+	int mass2[8];
+	int GodBless = 1;
+	for (int i = 0; i < 8; i++) {
+		mass2[i] = GodBless;
+		GodBless += 3;
+		cout << mass2[i] << endl;
+
+	}
+
+
+
+
+}
+
+//so hard to live when you stupid//
+
+
+
+
+
+
+
+
+
+
+
